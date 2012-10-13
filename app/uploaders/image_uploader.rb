feff7b13070e1bdc2dcd25ff6622ca9d5707020e
Lucca Mordente <luccamordente@gmail.com>
2012-10-13 11:15:28 -0300

    ajuste

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,15 +2,15 @@
 
 class ImageUploader < CarrierWave::Uploader::Base
 
+  # Include RMagick or MiniMagick support:
+  include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
+
   INTENT  = 'Relative'
   PROFILE = "#{Rails.root}/lib/profiles/srgb.icm"
 
   FORMATS_WHITELIST     = [ 'JPEG' ]
   COLORSPACES_WHITELIST = [ Magick::RGBColorspace, Magick::SRGBColorspace ]
-
-  # Include RMagick or MiniMagick support:
-  include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
